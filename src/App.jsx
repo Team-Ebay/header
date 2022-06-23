dda292180287ffe855857a11c54626198ce7fc89
Charles Stevens <cs-interface@protonmail.ch>
2022-06-23 17:07:11 +0000

    Trying to fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ function App() {
     <>
       <div className="navbar">
         {/* Holds the icon, search-by-category, search bar, search button */}
+        <a href="/">
+          <img id="ebay-icon" alt="eBay Logo" src="https://ir.ebaystatic.com/rs/v/fxxj3ttftm5ltcqnto1o4baovyl.png"/>
+        </a>
         <table>
           <tbody>
-            <tr id="ebay-icon">
-              <a href="/">
-                <img alt="eBay Logo" src="https://ir.ebaystatic.com/rs/v/fxxj3ttftm5ltcqnto1o4baovyl.png"/>
-              </a>
+            <tr id="ebay-icon-placeholder">
+
             </tr>
             <tr>
               Shop by category
